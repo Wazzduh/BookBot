3771f131118339b093c393a25bfcae921d4743f7
Wazz <Wazzduh@gmail.com>
2025-08-10 20:39:33 -0700

    made the code pretty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from stats import counts_words
 from stats import counts_char
+from stats import sorting
 
 def get_book_text (path_to_file):
     with open(path_to_file) as f:
@@ -7,12 +8,14 @@ def get_book_text (path_to_file):
         file_contents = f.read()
         return(file_contents)
 
-def main ():    
+def main ():   
+    library_of_letters = {} 
     path_to_file = "books/frankenstein.txt"
     contents = get_book_text(path_to_file)
     word_count = counts_words(contents)
-    print(f"{word_count} words found in the document")
     library_of_letters = counts_char(contents)
-    print(library_of_letters)
-
+    sorted_library = sorting(library_of_letters)
+    print(f"Found {word_count} total words")
+    for key, value in sorted_library.items():
+        print(f"{key}: {value}")
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,4 @@
+from operator import itemgetter
 
 def counts_words (contents):
     word_count = 0
@@ -13,6 +14,11 @@ def counts_char (contents):
         if char not in library:
             library[char] = 1  
         else:
-            library[char] += 1
-
+            library[char] += 1 
     return library
+
+def sorting(library):
+    alpha = {key: value for key, value in library.items() if key.isalpha()}
+    sorted_item = sorted(alpha.items(), key=lambda item: item[1], reverse=True)
+    sorted_dict = dict(sorted_item)
+    return sorted_dict
